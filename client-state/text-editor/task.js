ff3f02f4b928570d5b37eeca1635aefd6047d464
Natalia Svetlitskaya <svetlitskaya73@gmail.com>
2025-05-15 21:47:47 +0300

    First task rev.1

--- client-state/text-editor/task.js
+++ client-state/text-editor/task.js
@@ -3,9 +3,7 @@ const editor = document.getElementById("editor");
 
 window.addEventListener("load", () => {
     const savedText = localStorage.getItem("storageKey");
-    if (savedText !== null) {
-        editor.value = savedText;
-    }
+    editor.value = savedText;
 });
 
 editor.addEventListener("input", () => {
